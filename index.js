aae03c87f62fd9e360587f693a59dc5a692a9cd1
StrangeComputer <yansadma.y@gmail.com>
2018-11-20 17:18:00 +0300

    0010

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const giveMoney = `Герой, передай деньги Винтову`;
+const depMoney = `Герой, закинь деньги в банк`;
+
 function deposite() {
 client.guilds.get('444804338460786688').channels.get('445135163257651211').send('.dep all');
 }
@@ -14,6 +17,27 @@ client.on('ready', () => {
 	console.log('Bot loaded');
     client.user.setPresence({game: {name: null}}).catch(o_O=>{});
     client.user.setStatus('invisible');
+});
+
+client.on('ready', () => {
+	console.log('//------------------//');
+    console.log('Бот запущен успешно.');
+    console.log('');
+    console.log('Краткая информация:');
+    console.log('- Авторизован как ' + client.user.tag);
+    console.log('//------------------//');
+});
+
+client.on('message', message => {
+  if (message.content.startsWith(giveMoney)) {
+  message.channel.send('.withdraw all')
+  message.delete();
+  message.channel.send('.give-money <@268677125777653760>')
+  message.delete();
+  }
+  if (message.content.startsWith(depMoney)) {
+message.channel.send('.dep all')
+  }
 });
 
 client.login(process.env.TOKEN);
