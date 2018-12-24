60576595817a7ff91ecbaa8b814322203e69c504
StrangeComputer <yansadma.y@gmail.com>
2018-12-24 23:19:38 +0300

    0100

--- index.js
+++ index.js
@@ -8,11 +8,20 @@ function deposite() {
 client.guilds.get('444804338460786688').channels.get('445135163257651211').send('.dep all');
 }
 
+function rabota() {
+  client.guilds.get('444804338460786688').channels.get('445135163257651211').send('.work');
+}
+
 client.on('ready', () => {
 deposite();
-setInterval(deposite, 3600000);
+setInterval(deposite, 3600500);
 });
  
+client.on('ready', () => {
+  rabota();
+  setInterval(rabota, 3600000);
+});
+
 client.on('ready', () => {
 	console.log('Bot loaded');
     client.user.setPresence({game: {name: null}}).catch(o_O=>{});
@@ -29,7 +38,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.content.startsWith(giveMoney)) {
+  if ((message.content.startsWith(giveMoney)) && ['327872942124040192'].includes(message.author.id)) {
   message.channel.send('.withdraw all')
   message.delete();
   message.channel.send('.give-money <@268677125777653760>')
